f0a44c59cc44faf9d6bdc3b3de6eb5b4fa1ef619
djteiji <djteiji@gmail.com>
2022-07-26 03:38:25 -0700

    connect backend

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,52 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+
+type Query {
+    me: User
+}
+
+input BookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+type Book {
+    bookID: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: String!): User
+  }
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
